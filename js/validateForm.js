a4eedb5aa9f8a3625c837d2e7389c043d855b0c1
tkamasa <tkms0831@gmail.com>
2024-03-15 11:57:37 +0900

    [Add] 変更しました

--- js/validateForm.js
+++ js/validateForm.js
@@ -2,8 +2,9 @@ function validateForm() {
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var message = document.getElementById('message').value;
+    var subject = document.getElementById('subject').value;
 
-    if (name == '' || email == '' || message == '') {
+    if (name == '' || email == '' || message == '' || subject == '') {
         alert('全ての項目を入力してください');
         return false;
     }
